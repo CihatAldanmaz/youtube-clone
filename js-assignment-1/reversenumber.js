3971ed3bfeeab57d46c30120f57671c62de9cc94
Cihat Aldanmaz <cihataldanmaz@Cihats-MacBook-Pro.local>
2020-09-10 20:19:04 -0400

    palindrome done

--- js-assignment-1/reversenumber.js
+++ js-assignment-1/reversenumber.js
@@ -1,3 +1,5 @@
+// Write a JavaScript function that reverse a number.
+//  Example x = 32243;Expected Output : 34223
 
 function reversenum(number){
 let strnumber = number.toString().split("").reverse().join("")
